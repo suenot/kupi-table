c2ef61bdc86cb8a10d0da093a968b00bd949ba41
Eugen Soloviov <suenot@gmail.com>
2019-03-03 00:41:22 +0300

    boilerplate: settings, data, sass, cconfig, test

---
+++ src/components/KupiTable/KupiTable.test.js
@@ -0,0 +1,12 @@
+import { shallowMount } from '@vue/test-utils'
+import KupiTable from '@/components/KupiTable.vue'
+
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
